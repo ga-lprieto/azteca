95bdc58c42cca26b5bd13f3edff82c0d5a5a9da9
ga-lprieto <lprieto@grupoassa.com>
2014-09-18 10:58:38 -0300

    Start Rework Visual

--- Azteca/Azteca/Models/Postulante.cs
+++ Azteca/Azteca/Models/Postulante.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,52 +10,68 @@ namespace Azteca.Models
     public class Postulante
     {
         [Required]
+        [DisplayName("Nombre")]
         [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string cv_name { get; set; }
 
         [Required]
+        [DisplayName("Apellido")]
         [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string cv_lastname { get; set; }
 
         [Required]
+        [DisplayName("E-Mail")]
+        [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "It's not a valid E-mail")]
         [DisplayFormat(ConvertEmptyStringToNull = false)]
         public string cv_email { get; set; }
 
         [Required(ErrorMessage = "Birth date cannot be empty")]
+        [DisplayName("Fecha de Nacimiento")]
         public DateTime cv_dateofbirth { get; set; }
 
         [Required]
+        [DisplayName("Pais")]
         public string cv_country { get; set; }
 
         [Required]
+        [DisplayName("Ciudad")]
         public string cv_city { get; set; }
 
         [Required]
+        [DisplayName("Carrera")]
         public string cv_degree { get; set; }
 
         public string cv_other { get; set; }
 
         [Required]
+        [DisplayName("Nivel de Ingles")]
         public string cv_english { get; set; }
 
+        [DisplayName("Como nos Encontraste")]
         public string cv_howto { get; set; }
 
         [Required]
+        [DisplayName("Disponibilidad")]
         public string cv_availability { get; set; }
 
         [Required]
+        [DisplayName("Disponibilidad para Viajar")]
         public string cv_travel_disp { get; set; }
 
         [Required]
+        [DisplayName("Areas")]
         public string[] cv_jobareas { get; set; }
 
+        [DisplayName("Otra Informacion")]
         public string cv_otherinfo { get; set; }
 
         [Required]
+        [DisplayName("Remuneracion Pretendida")]
         public string cv_salary { get; set; }
 
         [Required]
+        [DisplayName("Curriculum Vitae")]
         public HttpPostedFileBase cv_file { get; set; }
     }
 }
